6015436fa78806a38a2763a0b3ffc7ec45be9d3b
angelopportunity <111150915+angelopportunity@users.noreply.github.com>
2023-04-07 16:45:09 -0700

    fixed cover urls

--- src/Data/portfolioData.js
+++ src/Data/portfolioData.js
@@ -1,85 +1,85 @@
 export default [
     {
         title: "Shared by Two",
-        imageURL: "src/Images/aya1.jpeg",
+        imageURL: "aya1.jpeg",
         genre: "SCIFI ROMANCE",
     },
     
     {
         title: "The Breeding Games",
-        imageURL: "src/Images/aya2.jpeg",
+        imageURL: "aya2.jpeg",
         genre: "SCIFI ROMANCE",
     },
     
     {
         title: "Alien Savage's Claim",
-        imageURL: "src/Images/aya3.jpeg",
+        imageURL: "aya3.jpeg",
         genre: "SCIFI ROMANCE",
     },
 
     {
         title: "Build Your Own Alien Boyfriend",
-        imageURL: "src/Images/aya4.jpeg",
+        imageURL: "aya4.jpeg",
         genre: "SCIFI ROMANCE",
     },
 
     {
         title: "The Breeding Prize",
-        imageURL: "src/Images/aya5.jpeg",
+        imageURL: "aya5.jpeg",
         genre: "SCIFI ROMANCE",
     },
 
     {
         title: "Knocked Up By Mr. Wrong",
-        imageURL: "src/Images/minx1.jpeg",
+        imageURL: "minx1.jpeg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "Grumpy Fake Husband",
-        imageURL: "src/Images/minx2.jpeg",
+        imageURL: "minx2.jpeg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "Knocked Up And Tied Down",
-        imageURL: "src/Images/minx3.jpg",
+        imageURL: "minx3.jpg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "How To Wife Your Nanny",
-        imageURL: "src/Images/minx4.jpeg",
+        imageURL: "minx4.jpeg",
         genre: "ROMANTIC COMEDY",
     },
 
     {
         title: "The Jerkhole Next Door",
-        imageURL: "src/Images/minx5.jpeg",
+        imageURL: "minx5.jpeg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "The Package Swap",
-        imageURL: "src/Images/minx6.jpeg",
+        imageURL: "minx6.jpeg",
         genre: "ROMANTIC COMEDY",
     },
 
     {
         title: "Grumpy Single Dad",
-        imageURL: "src/Images/minx7.jpeg",
+        imageURL: "minx7.jpeg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "Dirty Bet",
-        imageURL: "src/Images/minx8.jpeg",
+        imageURL: "minx8.jpeg",
         genre: "CONTEMPORARY ROMANCE",
     },
 
     {
         title: "Dom's Baby",
-        imageURL: "src/Images/minx9.jpg",
+        imageURL: "minx9.jpg",
         genre: "CONTEMPORARY ROMANCE",
     },
 ]
